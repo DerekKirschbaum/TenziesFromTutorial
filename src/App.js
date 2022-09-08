b735d761b22d4bec040de0cd8bb0e513d7a51f15
DerekKirschbaum <dgkirschbaum@gmail.com>
2022-09-07 20:41:01 -0700

    Fixed test changes

--- src/App.js
+++ src/App.js
@@ -65,7 +65,6 @@ export default function App() {
     
     return (
         <main>
-            <h1 className="title">Testing Changes</h1>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
